4361df80cfcb6bae8d1712efff076468ef330bf4
Maarten Huijsmans <maarten@lukin.be>
2013-10-26 22:28:34 +0100

    foursquare cached stuff

--- apis/test.js
+++ apis/test.js
@@ -2,9 +2,11 @@ var zalando = require('./zalando');
 //zalando.init_cache();
 
 // Print some cached items
-var cache = require('./cached.json');
+var cache = require('./zalando_cached.json');
+var cache2 = require('./foursquare_cached.json');
 console.log(cache['filter-top']);
 console.log(cache['filter-bottom']);
 console.log(cache['filter-shoes']);
+console.log(cache2);
 
 
