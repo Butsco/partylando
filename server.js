250ad006fc5ac0c42b4be74282084b7e5aa89e07
Bert Wijnants <bert.wijnants@gmail.com>
2013-10-26 15:18:58 +0200

    frontend setup

--- server.js
+++ server.js
@@ -2,16 +2,13 @@ var express = require("express");
 var http = require("http");
 
 var app = express();
+app.use(express.static('public'));
 var server = http.createServer(app)
 
 var io = require('socket.io').listen(server)
 
 io.on('connection',function(socket){
   socket.emit('init',{msg:"test"})
-});
-
-app.get('/', function(req, res){
-  res.send('hello world');
 });
 
 app.listen(3000);
